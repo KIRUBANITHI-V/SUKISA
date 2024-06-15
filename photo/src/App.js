f4ef87c674c3288cdd6764d16848314647143245
KIRUBANITHI V <141173903+KIRUBANITHI-V@users.noreply.github.com>
2024-06-15 17:35:26 +0530

    Update App.js

--- photo/src/App.js
+++ photo/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './login';
+import Root from './root';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Root/>
     </div>
   );
 }
